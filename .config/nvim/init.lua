f21859d867846a31b0cc562e69c581eece0e55a8
balaji <balaji.g@360hvpl.com>
2025-09-01 22:19:43 +0530

    Highlight on yanking

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,15 @@
 require("config.options")
 require("config.keybinds")
 require("config.lazy")
+
+local yank_group = vim.api.nvim_create_augroup("highlight_yank", { clear = true })
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+    group = yank_group,
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = "IncSearch", -- highlight group to use
+            timeout = 200,         -- duration in ms
+        })
+    end,
+})
