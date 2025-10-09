ff8bc3bfef5208491dda88e6306622ad2fbf4173
balaji <balaji.g@360hvpl.com>
2025-10-09 15:31:56 +0530

    some fixes

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,4 +1,3 @@
-local vim = vim
 return {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.8',
@@ -17,16 +16,28 @@ return {
         vim.keymap.set('n', '<leader>p', function()
             builtin.find_files({ hidden = true, no_ignore = true })
         end, { desc = "Find ALL files (incl. ignored)" })
+
+        -- default live grep
         vim.keymap.set('n', '<leader>ff', builtin.live_grep, { desc = 'Telescope live grep' })
+
+        -- No need to type escape character("\") while searching with special characters
+        vim.keymap.set('n', '<leader>fF', function()
+            builtin.live_grep({
+                additional_args = function() return { "--fixed-strings" } end
+            })
+        end, { desc = 'Literal live grep (no regex)' })
+        vim.keymap.set('n', '<leader>fs', builtin.grep_string, { desc = 'Telescope grep string' })
         vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
         vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
         vim.keymap.set('n', '<leader>fm', builtin.man_pages, { desc = 'Telescope help man pages' })
+        vim.keymap.set('n', '<leader>fgf', builtin.git_status, { desc = 'Telescope git files' })
         vim.keymap.set('n', '<leader>fn', function()
             builtin.find_files {
                 cmd = vim.fn.stdpath("config")
             }
         end)
 
+        -- Find references
         vim.keymap.set('n', '<leader>fr', function()
             builtin.lsp_references()
         end)
--- cht.sh
+++ cht.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-languages=`echo "golang lua typescript javascript nodejs" | tr ' ' '\n'`
+languages=`echo "javascript nodejs python golang lua typescript" | tr ' ' '\n'`
 core_utils=`echo "xargs find mv" | tr ' ' '\n'`
 
 selected=`printf "$languages\n" | fzf`
